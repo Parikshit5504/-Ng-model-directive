d5966fae04a0bc4e152d586fbfd92d6d221abd12
Parikshit5504 <desaip932@gmail.com>
2021-06-22 09:01:57 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo10';
+
+  name = 'Parikshit desai';
+email = 'desaip9321@gmail.com'
+contact = '11223344'
 }
